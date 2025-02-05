66ffe7288244f5350eeb9acb5b8d3185ced11864
Jennie <jenniean2002@gmail.com>
2025-02-05 16:26:12 -0500

    fix metrics

--- model_code/CNN.py
+++ model_code/CNN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
 import numpy as np
 from datasets import load_dataset
 import numpy as np
@@ -105,6 +106,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+loss_values = []
 for epoch in range(N_EPOCHS):
     model.train()
     running_loss = 0.0
@@ -116,10 +118,13 @@ for epoch in range(N_EPOCHS):
         optimizer.step()
         running_loss += loss.item()
 
-    print(f"Epoch {epoch + 1}/{N_EPOCHS}, Loss: {running_loss / len(train_loader):.4f}")
+    avg_loss = running_loss / len(train_loader)
+    loss_values.append(avg_loss)
+
+    print(f"Epoch {epoch + 1}/{N_EPOCHS}, Loss: {avg_loss:.4f}")
     
 # save the trained model
-torch.save(model.state_dict(), "C:/Users/jenni/virtualEnv/DS Capstone/DS_Capstone/model_results/vision_transformer_model_basic.pth")
+torch.save(model.state_dict(), "DS_Capstone/model_results/vision_transformer_model_basic.pth")
 
 # evaluation
 def evaluate_model(model, test_loader, device):
@@ -139,49 +144,89 @@ def evaluate_model(model, test_loader, device):
             all_preds.extend(predicted.cpu().numpy())
             all_probs.extend(probs.cpu().numpy())
 
+    # compute confusion matrix for 4 classes
+    confusion = confusion_matrix(all_labels, all_preds, labels=[0, 1, 2, 3])
+    
     # metrics
-    tn, fp, fn, tp = confusion_matrix(all_labels, all_preds).ravel()
     accuracy = accuracy_score(all_labels, all_preds)
     precision = precision_score(all_labels, all_preds, average='macro')
     f1 = f1_score(all_labels, all_preds, average='macro')
 
-    tpr = tp / (tp + fn) 
-    fpr = fp / (fp + tn)  
-    tnr = tn / (tn + fp)  
-    fnr = fn / (fn + tp)
+    # calculate TPR, FPR, TNR, FNR for each class
+    tpr_list = []
+    fpr_list = []
+    tnr_list = []
+    fnr_list = []
+    
+    for i in range(confusion.shape[0]):
+        tp = confusion[i, i]
+        fn = confusion[i, :].sum() - tp
+        fp = confusion[:, i].sum() - tp
+        tn = confusion.sum() - (tp + fn + fp)
+        
+        tpr = tp / (tp + fn)  
+        fpr = fp / (fp + tn)  
+        tnr = tn / (tn + fp)  
+        fnr = fn / (fn + tp)  
 
-    # roc curve
-    all_labels_bin = label_binarize(all_labels, classes=[0, 1, 2, 3])
+        tpr_list.append(tpr)
+        fpr_list.append(fpr)
+        tnr_list.append(tnr)
+        fnr_list.append(fnr)
+
+    # average TPR, FPR, TNR, FNR across all classes
+    avg_tpr = np.mean(tpr_list)
+    avg_fpr = np.mean(fpr_list)
+    avg_tnr = np.mean(tnr_list)
+    avg_fnr = np.mean(fnr_list)
+
+    # ROC Curve and AUC
+    all_labels_bin = label_binarize(all_labels, classes=[0, 1, 2, 3])  
     auc = roc_auc_score(all_labels_bin, np.array(all_probs), multi_class='ovr', average='macro')
     fpr_class, tpr_class, _ = roc_curve(all_labels_bin.ravel(), np.array(all_probs).ravel())
-    
-    # plot roc curve
+
+    # plot ROC Curve
     plt.figure()
     plt.plot(fpr_class, tpr_class, label='ROC curve (area = %0.2f)' % auc)
-    plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+    plt.plot([0, 1], [0, 1], linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic')
+    plt.title('ROC Curve')
     plt.legend(loc="lower right")
-    plt.savefig("C:/Users/jenni/virtualEnv/DS Capstone/DS_Capstone/model_results/cnn_roc_curve.png")
+    plt.savefig("DS_Capstone/model_results/cnn_roc_curve.png")
     plt.close()
 
-    # save metrics to csv
+    # save metrics to CSV
     metrics_dict = {
         "Accuracy": [accuracy],
         "Precision": [precision],
         "F1-Score": [f1],
-        "TPR (Sensitivity)": [tpr],
-        "FPR": [fpr],
-        "TNR (Specificity)": [tnr],
-        "FNR": [fnr],
+        "Avg TPR (Sensitivity)": [avg_tpr],
+        "Avg FPR": [avg_fpr],
+        "Avg TNR (Specificity)": [avg_tnr],
+        "Avg FNR": [avg_fnr],
         "AUC": [auc]
     }
 
     metrics_df = pd.DataFrame(metrics_dict)
-    metrics_df.to_csv("C:/Users/jenni/virtualEnv/DS Capstone/DS_Capstone/model_results/cnn_metrics_with_auc.csv", index=False)
+    metrics_df.to_csv("DS_Capstone/model_results/cnn_metrics_with_auc.csv", index=False)
+
+    # save confusion matrix plot
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.imshow(confusion, interpolation='nearest', cmap=plt.cm.Blues)
+    ax.set_title('Confusion Matrix')
+    ax.set_xlabel('Predicted Label')
+    ax.set_ylabel('True Label')
+    ax.set_xticks(np.arange(4))
+    ax.set_yticks(np.arange(4))
+    ax.set_xticklabels([0, 1, 2, 3])
+    ax.set_yticklabels([0, 1, 2, 3])
+    plt.colorbar(ax.imshow(confusion, interpolation='nearest', cmap=plt.cm.Blues))
+    plt.tight_layout()
+    plt.savefig("DS_Capstone/model_results/cnn_confusion_matrix.png")
+    plt.close()
 
 # run evaluation
 evaluate_model(model, test_loader, device)
