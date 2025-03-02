d6c15ad20f20b53bc3e016458a15d362ea287238
Jennie <jenniean2002@gmail.com>
2025-03-02 16:01:58 -0500

    results

--- model_code/vision_transformer.py
+++ model_code/vision_transformer.py
@@ -47,8 +47,8 @@ def run_transformer(
 # run grid search 
 # uncomment below to run grid search
 # Note: Change values below to be desired save path and search parameters
-SAVE_PATH = "model_results/transformer_v1/transformer_min_max/min_max_imbalanced"
-PREPROCESS = 'min_max'
+SAVE_PATH = "model_results/transformer_v1/transformer_z_score/z_score_balanced"
+PREPROCESS = 'z_score'
 
 # DROPOUT = [0.1, 0.3, 0.5]
 # LR = [0.001, 0.0001]
@@ -61,7 +61,7 @@ DROPOUT = [0.1]
 LR = [0.0001]
 EPOCHS = [50]
 BATCH_SIZE = [32]
-BALANCED = False
+BALANCED = True
 
 # experiment #
 COUNT = 1
