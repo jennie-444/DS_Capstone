b0ce6ed7fef1e9d9d321e6981a4855b245dc25df
Jennie <jenniean2002@gmail.com>
2025-03-02 11:04:25 -0500

    results

--- model_code/CNN.py
+++ model_code/CNN.py
@@ -71,8 +71,8 @@ def run_cnn(
 
 # run grid search
 # Note: Change values below to be desired save path, search parameters, and preprocessing technique
-SAVE_PATH = "model_results/cnn_v1/cnn_min_max/min_max_balanced"
-PREPROCESS = 'min_max'
+SAVE_PATH = "model_results/cnn_v1/cnn_z_score/z_score_balanced"
+PREPROCESS = 'z_score'
 
 # DROPOUT = [0.1, 0.3, 0.5]
 # LR = [0.001, 0.0001]
