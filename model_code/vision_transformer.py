2202706c6e123249ead8be6226b784595fb635f4
Jennie <jenniean2002@gmail.com>
2025-03-02 18:48:52 -0500

    results

--- model_code/vision_transformer.py
+++ model_code/vision_transformer.py
@@ -47,8 +47,8 @@ def run_transformer(
 # run grid search 
 # uncomment below to run grid search
 # Note: Change values below to be desired save path and search parameters
-SAVE_PATH = "model_results/transformer_v1/transformer_z_score/z_score_imbalanced"
-PREPROCESS = 'z_score'
+SAVE_PATH = "model_results/transformer_v1/transformer_local_contrast/local_contrast_imbalanced"
+PREPROCESS = 'local_contrast'
 
 # DROPOUT = [0.1, 0.3, 0.5]
 # LR = [0.001, 0.0001]
