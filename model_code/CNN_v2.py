2f1368656c5583623138f53a64716182e4ca847d
Jennie <jenniean2002@gmail.com>
2025-03-11 23:30:18 -0400

    experiments

--- model_code/CNN_v2.py
+++ model_code/CNN_v2.py
@@ -93,8 +93,8 @@ def run_cnn(
 
 # run grid search
 # Note: Change values below to be desired save path, search parameters, and preprocessing technique
-SAVE_PATH = "model_results/cnn_v2/cnn_raw/raw_balanced"
-PREPROCESS = None
+SAVE_PATH = "model_results/cnn_v2/cnn_bilateral_filter/bilateral_filter_imbalanced"
+PREPROCESS = "bilateral_filter"
 
 # DROPOUT = [0.1, 0.3, 0.5]
 # LR = [0.001, 0.0001]
@@ -107,7 +107,7 @@ DROPOUT = [0.5]
 LR = [0.0001]
 EPOCHS = [50]
 BATCH_SIZE = [32] 
-BALANCED = True
+BALANCED = False
 
 # experiment #
 COUNT = 1
