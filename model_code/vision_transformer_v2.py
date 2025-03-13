2aeb9fa385f77a1d6eca296e01199d135de056c6
Jennie <jenniean2002@gmail.com>
2025-03-13 08:57:39 -0400

    exp

--- model_code/vision_transformer_v2.py
+++ model_code/vision_transformer_v2.py
@@ -47,7 +47,7 @@ def run_transformer(
 # run grid search 
 # uncomment below to run grid search
 # Note: Change values below to be desired save path and search parameters
-SAVE_PATH = "model_results/transformer_v2/transformer_bilateral_filter/bilateral_filter_balanced"
+SAVE_PATH = "model_results/transformer_v2/transformer_bilateral_filter/bilateral_filter_imbalanced"
 PREPROCESS = 'bilateral_filter'
 
 # DROPOUT = [0.1, 0.3, 0.5]
@@ -61,7 +61,7 @@ DROPOUT = [0.1]
 LR = [0.0001]
 EPOCHS = [50]
 BATCH_SIZE = [32]
-BALANCED = True
+BALANCED = False
 
 # experiment #
 COUNT = 1
