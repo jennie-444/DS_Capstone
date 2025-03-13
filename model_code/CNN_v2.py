b93a3eaf29ce3dbd421cbb2e6c594bf795fb2f59
Jennie <jenniean2002@gmail.com>
2025-03-12 22:34:18 -0400

    experiments

--- model_code/CNN_v2.py
+++ model_code/CNN_v2.py
@@ -93,8 +93,8 @@ def run_cnn(
 
 # run grid search
 # Note: Change values below to be desired save path, search parameters, and preprocessing technique
-SAVE_PATH = "model_results/cnn_v2/cnn_median_blur/median_blur_imbalanced"
-PREPROCESS = "median_blur"
+SAVE_PATH = "model_results/cnn_v2/cnn_min_max/min_max_balanced"
+PREPROCESS = "min_max"
 
 # DROPOUT = [0.1, 0.3, 0.5]
 # LR = [0.001, 0.0001]
@@ -107,7 +107,7 @@ DROPOUT = [0.5]
 LR = [0.0001]
 EPOCHS = [50]
 BATCH_SIZE = [32] 
-BALANCED = False
+BALANCED = True
 
 # experiment #
 COUNT = 1
